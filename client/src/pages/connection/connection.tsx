68be060f81be31cdebe1a6f7c5072d405246f9df
Eliott-web <he202397@students.ephec.be>
2026-04-18 08:44:34 +0200

    update :  page connection

--- client/src/pages/connection/connection.tsx
+++ client/src/pages/connection/connection.tsx
@@ -1,13 +1,38 @@
 import { useNavigate } from "react-router-dom"
-import "./connection.css"
+import style from "./connection.module.css"
 
 function Connec() {
     const navigate = useNavigate()
     return (
         <div>
-            <h1>page de connection</h1>
-            <button onClick={function () {navigate("/inscription")}}>inscription</button>
-            <button onClick={function () {navigate("/")}}>retours</button>
+            <header>
+                <div className="{style.logoContainer}">
+                    <img src="./Logo_electronique_dynamique_avec_ampoule.png" alt="logo" />
+                    <h2>ProjetHub</h2>
+                </div>
+
+                <div>
+                    <button className={style.btnConnection} onClick={function () {navigate("/")}}>Accueil</button>
+                </div>
+            </header>
+
+            <main className={style.mainConnection}>
+                <div>
+                    <h3>Bon retour !</h3>
+                    <form action="">
+                        <div>
+                            <label htmlFor="email">Email : </label>
+                            <input type="email" id="email" name="email" placeholder="Entrez votre email" required />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Mot de passe : </label>
+                            <input type="password" id="password" name="password" placeholder="••••••••" required />
+                        </div>
+                        <button className={style.btnConnection} type="submit">Se connecter</button>
+                    </form>
+                    <p>Vous n'avez pas de compte ? <button className={style.btnConnection} onClick={function () {navigate("/inscription")}}>Créer un compte</button></p>
+                </div>
+            </main>
         </div>
     )
 }
