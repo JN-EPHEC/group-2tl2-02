0c044a6d7bd19708fef23bf96d25dc7efc72f660
Eliott-web <he202397@students.ephec.be>
2026-05-07 16:24:51 +0200

    add : création de la page panier beta #54

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Projet from "./pages/projets/projet"
 import ConContact from "./pages/info/contact"
 import APropos from "./pages/info/a_propos"
 import Faq from "./pages/info/faq"
+import Panier from "./pages/panier/panier"
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/contact" element={<ConContact />} />
         <Route path="/a_propos" element={<APropos />} />
         <Route path="/faq" element={<Faq />} />
+        <Route path="/panier" element={<Panier />} />
       </Routes>
     </BrowserRouter>
   )
