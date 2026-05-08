835ab492eb528dfe8f1cd34667aafc798bee372e
Eliott-web <he202397@students.ephec.be>
2026-05-08 17:49:25 +0200

    Stop tracking database.ts and ignore it

--- server/src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('mon_projet_db', 'postgres', 'X3IRAYHAMIDE2006@', {
-    host: 'localhost',
-    dialect: 'postgres',
-    logging: false,
-});
-
-export default sequelize;
