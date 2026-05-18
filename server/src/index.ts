cd1d3b078041d8b2282db82657cea7c482670348
Rayan K <r.kinet@students.ephec.be>
2026-05-18 10:53:20 +0200

    modif alter #33

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { displayBanner } from './startup';
+
 import express from 'express';
 import { User, Project, Image } from './models/lien_inter/index' // Vérifie bien que le nom du fichier est correct
 import sequelize from './config/database';
@@ -23,13 +23,13 @@ app.get('/api/data', (req, res) => {
 });
 
 // --- LE BLOC DE SYNCHRO (C'est ici que tout se joue) ---
-sequelize.sync({ force: true })
+sequelize.sync({ alter: true })
     .then(() => {
         console.log("✅ Base de données PostgreSQL synchronisée");
 
         // On lance le serveur SEULEMENT quand la DB est prête
         app.listen(port, () => {
-            console.log(displayBanner()); // On affiche la bannière dans la console
+           
             console.log(`🚀 Serveur lancé sur : http://localhost:${port}/api/users`);
         });
     })
